730e55ba0757afa2f8ce9e8c50eac86c056be40d
Ralph Ocs <rap.ocba@gmail.com>
2022-12-07 13:49:14 +0800

    navbar & banner

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
 
-const ui = new UI();
+// navBtn animate when click
+const navBtn = document.querySelector('.navBtn');
+const navbar = document.querySelector('.navbar')
+navBtn.addEventListener('click', ()=>{
 
+    // navBtn animate when click
+    navBtn.classList.toggle('change')
+    // navbar collapse at left sidebar
+    navbar.classList.toggle('nav-show')
 
-function myFunction(x) {
-    x.classList.toggle("change");
+});
+
+// Video btn switch
+let swtch = document.querySelector(`.video_switch`);
+let btn = document.querySelector(`.video_switch-btn`);
+
+swtch.addEventListener(`click`,function(){  
+   btn.classList.toggle(`btnSlide`);
+  if(!btn.classList.contains(`btnSlide`)){
+    document.querySelector(`.ban_item`).play()
+  }else{
+    document.querySelector(`.ban_item`).pause()
   }
+   
+})
 
-function eventListeners(){
-    document.querySelector('.navBtn').addEventListener('click',function(){
-        ui.showNav();
-    })
-}
-
-function UI(){}
-
-
-UI.prototype.showNav = function(){
-    document.querySelector('.navbar').classList.toggle('nav-show');
-}
-
-eventListeners();
